fefcef1a42ddff63c0277a516d4545e1e0d4ae8c
preetham-p-m <preethampm1@gmail.com>
2023-09-18 20:38:40 +0530

    added mode and mapped table

---
+++ src/main/java/com/ppm/springrestapi/model/CloudVendor.java
@@ -0,0 +1,61 @@
+package com.ppm.springrestapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cloud_vendor")
+public class CloudVendor {
+
+  @Id
+  public String id;
+
+  public String name;
+
+  public String address;
+
+  public String phoneNumber;
+
+  public CloudVendor() {
+  }
+
+  public CloudVendor(String id, String name, String address, String phoneNumber) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}
